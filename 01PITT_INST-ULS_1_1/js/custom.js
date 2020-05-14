557ee8010c25e2ae9197083745adcc641c2378ae
Alex Wreschnig <alex.wreschnig@gmail.com>
2020-05-14 13:57:25 -0400

    Executing JS after page load, plus a method for removing a target attribute. Code is not yet running on page *rebuild* yet. Aria label rewriting is also not quite working.

--- 01PITT_INST-ULS_1_1/js/custom.js
+++ 01PITT_INST-ULS_1_1/js/custom.js
@@ -15,25 +15,42 @@ var pittJS = function() {
 		gtag('config', 'UA-129368242-1');
 	}
 
+	/*
+	 *  Removes the target attribute from the new search link.
+	 *  Will also reset the aria label to be correct (if it's in English)
+	 */
+	function newSearchSameTab() {
+		var newSearchLink = document.querySelector('[data-main-menu-item="NewSearch"] a');
+		newSearchLink.removeAttribute("target");
+
+		if(newSearchLink.getAttribute("aria-label") == "New Search, opens in a new window") {
+			newSearchLink.setAttribute("aria-label", "New Search");
+		}
+	}
+
 	function helloWorld() {
 		app.component('prmTopBarBefore', {
 			template: `<span style="margin-left: 40%;">Hello World</span>`
 		});
 	}
 
-	function private_setup() {
+	function privateSetup() {
 		app = angular.module('viewCustom', ['angularLoad']);
 		console.log("Executing custom JS.");
 
 		//helloWorld();
-		addGoogleAnalytics();
+		angular.element(function () {
+    		console.log('page loading completed');
+    		addGoogleAnalytics();
+			newSearchSameTab();
+		});
 		
 		return;
 	}
 
 	return {
 		setupConstructor: function() {
-			private_setup();
+			privateSetup();
 		}
 	}
 
