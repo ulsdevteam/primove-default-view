5382c96b8d2c7d129f033d04a190ee990fbd279b
Alex Wreschnig <alex.wreschnig@gmail.com>
2021-10-19 19:36:36 -0400

    I have the button working. Mostly. There appear to be some race conditions around rebuilding the button itself, and around the filters that may not appear after some button actions.

--- 01PITT_INST-ULS_1_8/js/custom.js
+++ 01PITT_INST-ULS_1_8/js/custom.js
@@ -38,7 +38,73 @@
 			}
 		}
 
+		/*
+		 * function checkAngularCheckbox(element)
+		 * function uncheckAngularCheckbox(element)
+		 * 
+		 * Check and uncheck a Primo VE style checkbox.
+		 * 
+		 * These changes have been reverse-engineered from
+		 * the existing Angular code and are what happens
+		 * when Primo itself checks (or unchecks) a
+		 * checkbox, as far as we can tell.
+		 */
+		function checkAngularCheckbox(element) {
+			// remove class ng-empty
+			element.classList.remove("ng-empty");
+			// add class ng-not-empty
+			element.classList.add("ng-not-empty");
+			// add class md-checked
+			element.classList.add("md-checked");
+			// change attribute aria-checked : set value to true
+			element.setAttribute("aria-checked", true);
+		}
+
+		function uncheckAngularCheckbox(element) {
+			// remove class ng-not-empty
+			element.classList.remove("ng-not-empty");
+			// remove class md-checked
+			element.classList.remove("md-checked");
+			// add class ng-empty
+			element.classList.add("ng-empty");
+			// change attribute aria-checked : set value to false
+			element.setAttribute("aria-checked", false);
+		}
+
+		/*
+		 * reShareIntegrationButton
+		 *
+		 * We're replacing the Expand to Non-Pitt Results button with our own custom creation
+		 * that points to the PittCatPlus search scope.
+		 */
 		function reShareIntegrationButton() {
+			/*
+				FIND: (?<=\&tab=)(?<tab>[^&#]+)(?=(?:\&|#|$))				REPLACE: \&tab=LibraryCatalog
+				FIND: (?<=\&search_scope=)(?<scope>[^&#]+)(?=(?:\&|#|$))	REPLACE: \&search_scope=
+			
+				Breaking down the regex:
+					(?<=\&tab=)			Positive Lookbehind: we don't want to return the part of the match containing &tab=
+
+					(?<tab>[^&#]+)		Named capture group: this contains the value in the name-value pair, basically
+											everything after the equals sign. To grab that, we're doing a lazy match on
+											everything that's not an ampersand. This will run until it sees another ampersand,
+											signifying the start of another name/value pair; a hash/pound sign, signifying
+											the start of an anchor; or the end of the location string.
+
+											This will be stored as tab and can be referenced later.
+
+					(?=(?:\&|#|$))		Positive Lookahead (and a non-capturing group): We don't want to include the delimiter
+											in the match (the &, #, or $) but we want to make sure it's there, so we're running
+											a positive lookahead to confirm it. Because we have three options (which I've
+											separated out with pipes rather than a character set) I put those inside parens
+											to limit possible side-effects. Parens create capture groups so I made this a non-
+											capturing group just to keep our capture groups clean.
+			*/
+			var tabRegexMatch = new RegExp('(?<=\&tab=)(?:[^&#]+)(?=(?:\&|#|$))')
+			var tabRegexReplace = new RegExp('(?<=\&tab=)(?<tab>[^&#]+)(?=(?:\&|#|$))')
+			var scopeRegexMatch = new RegExp('(?<=\&search_scope=)(?:[^&#]+)(?=(?:\&|#|$))')
+			var scopeRegexReplace = new RegExp('(?<=\&search_scope=)(?<scope>[^&#]+)(?=(?:\&|#|$))')
+
 			var expandButton = document.querySelector('div.sidebar-section > md-checkbox');
 			if (expandButton) {
 				if(!expandButton.classList.contains("uls-replaced")) {
@@ -54,77 +120,22 @@
 
 					// Setup the event listeners
 					expandButton.addEventListener("click", function() {
-						// These changes have been reverse-engineered from
-						// the existing Angular code and are what happens
-						// when a checkbox is checked (or unchecked).
-
 						// Is this already checked? We can look at
 						// ng-empty (unchecked) vs. ng-not-empty (checked)
 						if(expandButton.classList.contains("ng-empty")) {
 							// checkbox is not yet checked
+							checkAngularCheckbox(this);
 
-							// remove class ng-empty
-							expandButton.classList.remove("ng-empty");
-							// add class ng-not-empty
-							expandButton.classList.add("ng-not-empty");
-							// add class md-checked
-							expandButton.classList.add("md-checked");
-							// change attribute aria-checked : set value to true
-							expandButton.setAttribute("aria-checked", true);
-
-							// Actual functionality goes here: change scope
-
-							/*
-								FIND: (?<=\&tab=)(?<tab>[^&#]+)(?=(?:\&|#|$))				REPLACE: \&tab=LibraryCatalog
-								FIND: \&search_scope=[^&]+?(\&|$)	REPLACE: \&search_scope=
-							
-								Breaking down the regex:
-									(?<=\&tab=)			Positive Lookbehind: we don't want to return the part of the match containing &tab=
-
-									(?<tab>[^&#]+)		Named capture group: this contains the value in the name-value pair, basically
-															everything after the equals sign. To grab that, we're doing a lazy match on
-															everything that's not an ampersand. This will run until it sees another ampersand,
-															signifying the start of another name/value pair; a hash/pound sign, signifying
-															the start of an anchor; or the end of the location string.
-
-															This will be stored as tab and can be referenced later.
-
-									(?=(?:\&|#|$))		Positive Lookahead (and a non-capturing group): We don't want to include the delimiter
-															in the match (the &, #, or $) but we want to make sure it's there, so we're running
-															a positive lookahead to confirm it. Because we have three options (which I've
-															separated out with pipes rather than a character set) I put those inside parens
-															to limit possible side-effects. Parens create capture groups so I made this a non-
-															capturing group just to keep our capture groups clean.
-							*/
-							var tabRegexMatch = new RegExp('(?<=\&tab=)(?:[^&#]+)(?=(?:\&|#|$))')
-							var tabRegexReplace = new RegExp('(?<=\&tab=)(?<tab>[^&#]+)(?=(?:\&|#|$))')
-							var scopeRegexMatch = new RegExp('(?<=\&search_scope=)(?:[^&#]+)(?=(?:\&|#|$))')
-							var scopeRegexReplace = new RegExp('(?<=\&search_scope=)(?<scope>[^&#]+)(?=(?:\&|#|$))')
-							console.log("Search Scope Regex = "+String(window.location).match(scopeRegexMatch));
-
-							// Store the previous value, just in case.
-							console.log("Printing the value of window.uls to the console.");
-							console.log(window.uls);
-							
-							window.uls.previousLocation = window.location;
-							window.uls.previousLocation.tab = String(window.location).match(tabRegexMatch);
-							window.uls.previousLocation.search_scope = String(window.location).match(scopeRegexMatch);
-							window.uls.nextLocation = String(window.location).replace(tabRegexReplace, "???").replace(scopeRegexReplace, "???");
-							
-							//window.location = window.uls.nextLocation;
+							// Change Scope
+							// Setting window.location changes the angular context, but it DOES force basically a full reload...
+							window.location = String(window.location).replace(tabRegexReplace, "WorldCatPlus").replace(scopeRegexReplace, "WORLDCATPLUS");
 						} else {
 							// implies that the checkbox is currently checked
+							uncheckAngularCheckbox(this);
 
-							// remove class ng-not-empty
-							expandButton.classList.remove("ng-not-empty");
-							// remove class md-checked
-							expandButton.classList.remove("md-checked");
-							// add class ng-empty
-							expandButton.classList.add("ng-empty");
-							// change attribute aria-checked : set value to false
-							expandButton.setAttribute("aria-checked", false);
-
-							// Actual functionality goes here: change scope
+							// Change Scope
+							// Setting window.location changes the angular context, but it DOES force basically a full reload...
+							window.location = String(window.location).replace(tabRegexReplace, "Everything").replace(scopeRegexReplace, "MyInst_and_CI");
 						}
 					});
 				} else {
@@ -138,6 +149,26 @@
 			*/
 			// just because
 			setTimeout(reShareIntegrationButton, 500); 
+		}
+
+		/*
+		 * Checking a checkbox if we're in the right scope
+		 */
+		function checkBoxIfInReshareScope() {
+			// Bad bad bad practice! Reused code. See description in reShareIntegrationButton().
+			var scopeRegexMatch = new RegExp('(?<=\&search_scope=)(?:[^&#]+)(?=(?:\&|#|$))')
+			var expandButton = document.querySelector('div.sidebar-section > md-checkbox');
+			var primoReShareScope = "WORLDCATPLUS";
+			if (expandButton) {
+				// Breaking up the if-statements to avoid null references
+				if(String(window.location).match(scopeRegexMatch) == primoReShareScope && expandButton.classList.contains("ng-empty")) {
+					// We're in the ReShare Scope, and the checkbox isn't checked.
+					// Add test for race condition here?
+					checkAngularCheckbox(expandButton);
+				}
+			}
+			// just because
+			setTimeout(checkBoxIfInReshareScope, 250); 
 		}
 
 		/* libanswers chat widget 
@@ -245,6 +276,7 @@
 
 			angular.element(function () {
 				console.log('page loading completed');
+				checkBoxIfInReshareScope();
 				addGoogleAnalytics();
 				prmSearchResultAvailabilityLineAfterTemplate();
 				chatWidget();
